ec47fd1054496496976645ca69598555068a321a
jamie ontiveros <jamie@ontiveros.me>
2025-10-12 23:10:33 -0400

    fix: Remove conflicting PHP CS Fixer rule
    - Remove 'single_blank_line_before_namespace' rule
    - Conflicts with 'blank_line_before_statement' rule
    - Resolves PHP CS Fixer configuration error

--- api/.php-cs-fixer.php
+++ api/.php-cs-fixer.php
@@ -113,7 +113,6 @@ return $config->setRules([
         'return_type_declaration' => true,
         'semicolon_after_instruction' => true,
         'short_scalar_cast' => true,
-        'single_blank_line_before_namespace' => true,
         'single_class_element_per_statement' => true,
         'single_import_per_statement' => true,
         'single_line_after_imports' => true,
